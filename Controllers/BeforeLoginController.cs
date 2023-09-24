93b4f5ca9b80fb4e3d87a4476dc7aa7da6113aee
Prabhu Lakhera <Prabhulakhera372@gmail.com>
2023-09-24 16:13:35 +0530

    Added new page User registraion

---
+++ Controllers/BeforeLoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medkart.com__An_online_medical_store_.Controllers
+{
+    public class BeforeLoginController : Controller
+    {
+        public IActionResult UserRegistration()
+        {
+            return View();
+        }
+    }
+}
